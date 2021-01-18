bd487b8972849e54d6a79702a74940388ec916ec
thehightech42 <thehightech42@gmail.com>
2021-01-18 01:06:50 +0100

    Modification de l'affichage de la page my-account

--- public/view/my-account.php
+++ public/view/my-account.php
@@ -15,34 +15,38 @@ ob_start();
         <div class="col-lg-4">
             <h3>Modification des informations</h3>
             <form action="/updateUserInformation" method="POST">
-                <label for="first_name">Prénom :
+                <div class="form-group">
+                    <label for="first_name">Prénom :</label>
                     <input type="text" name="first_name" class="input form-control" placeholder="Votre prénom" value="<?php 
                         if(isset($userInformation['first_name'])){
                             echo $userInformation['first_name'];
                         }
-                        ?>">
-                </label>
-                <label for="last_name">Nom :
+                    ?>">
+                </div>
+                <div class="form-group">
+                    <label for="last_name">Nom :</label>
                     <input type="text" name="last_name" class="input form-control" placeholder="Votre nom" value="<?php 
                         if(isset($userInformation['last_name'])){
                             echo $userInformation['last_name'];
                         }
-                        ?>">
-                </label>
-                <label for="email">Email :
+                    ?>">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email :</label>
                     <input type="email" name="email" class="input form-control" placeholder="Votre email" value="<?php 
                         if(isset($userInformation['email'])){
                             echo $userInformation['email'];
                         }
-                        ?>"required>
-                </label>
-                <label for="pseudo">Pseudo :
+                    ?>"required>
+                </div>
+                <div class="form-group">
+                    <label for="pseudo">Pseudo :</label>
                     <input type="text" name="pseudo" class="input form-control" placeholder="Votre pseudo" value="<?php 
                         if(isset($userInformation['pseudo'])){
                             echo $userInformation['pseudo'];
                         }
-                        ?>" required>
-                </label>
+                    ?>" required>
+                </div>
                 <input type="submit" name="submit" value="Enregistrer les modifications">
             </form>
             <p><?php if(isset($_SESSION['infoUserUpdate']) && $_SESSION['infoUserUpdate'] !== false ){echo $_SESSION['infoUserUpdate'];}; unset($_SESSION['infoUserUpdate'])?></p>
@@ -50,15 +54,19 @@ ob_start();
         <div class="col-lg-4">
             <h3>Changement du mot de passe</h3>
             <form id="formUpdatePassword" method="POST" action="/updatePassword" >
-                <label for="lastPassword">Ancien mot de passe :
+                <div class="form-group">
+                    <label for="lastPassword">Ancien mot de passe :</label>
                     <input type="password" name="lastPassword" class="input form-control" placeholder="Votre ancien mot de passe">
-                </label>
-                <label for="password1">Votre nouveau mot de passe :
+                </div>
+                <div class="form-group">
+                    <label for="password1">Votre nouveau mot de passe :</label>
                     <input type="password" id="inputPassword1" name="password1" class="input form-control" placeholder="Votre nouveau mot de passe">
-                </label>
-                <label for="password2">Confirmez votre nouveau mot de passe :
+                </div>
+                <div class="form-group">
+                    <label for="password2">Confirmez votre nouveau mot de passe :</label>
                     <input type="password" id="inputPassword2"name="password2" class="input form-control" placeholder="Confirmez votre nouveau mot de passe">
-                </label>
+                </div>
+
                 <input type="submit" name="submit" value="Changer votre mot de passe">
             </form>
                 <?php if(isset($_SESSION['userPasswordInfo'])){echo $_SESSION['userPasswordInfo']; unset($_SESSION['userPasswordInfo']);}?>
